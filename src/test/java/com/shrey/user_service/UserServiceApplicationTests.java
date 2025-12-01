866af829569eb801db5e321bcd37623809d89f04
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2025-12-01 22:08:01 +0530

    Initialised repo

---
+++ src/test/java/com/shrey/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrey.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
