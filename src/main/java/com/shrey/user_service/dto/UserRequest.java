37131f66c0e675431e0484ffc4a5156e88086ca3
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2025-12-02 16:44:30 +0530

    added validation to UserRequest dto

--- src/main/java/com/shrey/user_service/dto/UserRequest.java
+++ src/main/java/com/shrey/user_service/dto/UserRequest.java
@@ -1,7 +1,14 @@
 package com.shrey.user_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserRequest {
+    @NotBlank(message = "name is required")
     private String name;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email must be valid")
     private String email;
 
     public UserRequest() {};
