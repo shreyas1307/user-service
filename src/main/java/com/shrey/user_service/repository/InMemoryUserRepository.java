365334ce228e0a0728d80c42ac0d2482e04c5949
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2025-12-05 23:24:16 +0530

    added new findUserById in repository

--- src/main/java/com/shrey/user_service/repository/InMemoryUserRepository.java
+++ src/main/java/com/shrey/user_service/repository/InMemoryUserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
@@ -40,6 +41,11 @@ public class InMemoryUserRepository implements UserRepository {
     public List<User> findAll() {
         return new ArrayList<>(storage.values());
     }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return Optional.ofNullable(storage.get(id));
+    };
 
     private Boolean nameContainsIgnoreCase(User user, String substring) {
         return user.getName() != null && user.getName().toLowerCase().contains(substring.toLowerCase());
--- src/main/java/com/shrey/user_service/repository/UserRepository.java
+++ src/main/java/com/shrey/user_service/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.shrey.user_service.repository;
 
 import com.shrey.user_service.model.User;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
     User save(User user);
     List<User> findByNameContaining(String name);
+    Optional<User> findUserById(Long id);
     List<User> findAll();
 }
