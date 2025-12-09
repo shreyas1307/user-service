2158e1dfe2c93cf353389d371a2e02c838bfbcef
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2025-12-09 18:47:52 +0530

    removed unused import

--- src/main/java/com/shrey/user_service/service/UserService.java
+++ src/main/java/com/shrey/user_service/service/UserService.java
@@ -8,7 +8,6 @@ import com.shrey.user_service.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
